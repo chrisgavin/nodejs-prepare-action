7806dae1f6671d801be28ce5871fa6dc391bb1b4
Chris Gavin <chris@chrisgavin.me>
2021-04-11 13:36:59 +0100

    Fix another issue in the bootstrap shim.

--- bootstrap-shim.js
+++ bootstrap-shim.js
@@ -2,13 +2,13 @@
 
 const childProcess = require("child_process");
 const fs = require("fs");
+const path = require("path");
 
-process.chdir(__dirname);
-const target = "./build/index.js";
+const target = path.join(__dirname, "build", "index.js");
 try {
 	if (!fs.existsSync(target)) {
 		console.warn("This is a development build! For greater reliability and faster startup time, please use a tagged release.");
-		childProcess.execFileSync("npm", ["run", "bootstrap"], {stdio: "inherit"});
+		childProcess.execFileSync("npm", ["run", "bootstrap"], {stdio: "inherit", cwd: __dirname});
 	}
 	childProcess.execFileSync(process.execPath, [target], {stdio: "inherit"});
 }
